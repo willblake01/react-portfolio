02e1b98d884fea78b2df0e82af7dd4faf32853b5
William Blake <willblakebooking@gmail.com>
2018-05-11 11:41:41 -0500

    new line wrap syntax guidelines

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -8,12 +8,19 @@ class Sidebar extends React.Component {
     return (
       <section className='sidebar'>
       <div id='connect'>
-        <h2>Connect with Me</h2>
-
-        <a href='https://github.com/WillBlake01' target='_blank'><img src={GitHub} className='social' alt='GitHub' /></a>
-        <a href='https://www.linkedin.com/in/william-blake/' target='_blank'><img src={LinkedIn} className='social' alt='LinkedIn' /></a>
-        <a href='https://stackoverflow.com/users/8656082/william-blake?tab=profile' target='_blank'><img src={StackOverflow} className='social' alt='Stack Overflow' /></a>
-      </div>
+          <h2>Connect with Me</h2>
+          <a href='https://github.com/WillBlake01' target='_blank' rel='noopener noreferrer'>
+            <img src={GitHub} className='social' alt='GitHub' />
+          </a>
+          <a href='https://www.linkedin.com/in/william-blake/' target='_blank'
+          rel='noopener noreferrer'>
+            <img src={LinkedIn} className='social' alt='LinkedIn' />
+          </a>
+          <a href='https://stackoverflow.com/users/8656082/william-blake?tab=profile'
+          target='_blank' rel='noopener noreferrer'>
+            <img src={StackOverflow} className='social' alt='Stack Overflow' />
+          </a>
+        </div>
     </section>
     );
   }
