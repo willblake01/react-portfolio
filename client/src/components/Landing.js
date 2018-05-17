8d811df0239e12c312138de258ddad4e29487c80
William Blake <willblakebooking@gmail.com>
2018-05-17 13:29:27 -0500

    landing page copy and styling

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,9 +7,14 @@ class Landing extends React.Component {
       <div className='masthead'>
         <div className='wrapper-landing'>
             <div className='content-landing'>
-              <div id='headline'>...and there is a way!</div>
-              <MernLogos />
+              <div class='copy' id='headline'>...and there is a way!</div>
+              <div class='copy' id='about-landing'>Hi, I'm Will. I'm a Full-Stack developer from
+              Austin, TX. I focus on MERN stack applications but my knowledge base spans way beyond
+              that, just ask!</div>
+              <div class='copy' id='learn-more'>Learn more about what I do</div>
+              <div class='copy' id='down-arrow'>&or;</div>
             </div>
+            <MernLogos />
         </div>
       </div>
     );
