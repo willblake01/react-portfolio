66a9bcca3a7b1373994bb9246b3af7883d6afd9c
William Blake <willblakebooking@gmail.com>
2018-05-17 15:59:11 -0500

    fixed syntax errors, setup store

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -10,21 +10,21 @@ class Contact extends React.Component {
       <div className='masthead'>
         <ReactHeader />
         <Navbar />
-        <div id='main-container' class='container'>
-          <section class='main-section'>
+        <div id='main-container' className='container'>
+          <section className='main-section'>
             <h1>Contact</h1>
             <form id='contact-form'>
               <ul>
                 <li>
-                  <label for='name'>Name</label>
+                  <label htmlFor='name'>Name</label>
                   <input type='text' id='name' name='name' placeholder='John Smith' required='required' />
                 </li>
                 <li>
-                  <label for='email'>Email</label>
+                  <label htmlFor='email'>Email</label>
                   <input type='email' id='email' name='email' placeholder='example@gmail.com' required='required' />
                 </li>
                 <li>
-                  <label for='message'>Message</label>
+                  <label htmlFor='message'>Message</label>
                   <textarea id='message' name='message' required='required'></textarea>
                 </li>
               </ul>
