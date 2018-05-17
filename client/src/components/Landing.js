c72f00fbfcb8fa463facef6c836366629b40a519
William Blake <willblakebooking@gmail.com>
2018-05-17 16:28:47 -0500

    added flexbox to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,9 +8,9 @@ class Landing extends React.Component {
         <div className='wrapper-landing'>
             <div className='content-landing'>
               <div className='copy' id='headline'>...and there is a way!</div>
-              <div className='copy' id='about-landing'>Hi, I'm Will. I'm a Full-Stack developer from
-              Austin, TX. I focus on MERN stack applications but my knowledge base spans way beyond
-              that, just ask!</div>
+              <div className='copy' id='about-landing'>Hi, I'm Will. I'm a Full-Stack developer
+              from Austin, TX. I focus on MERN stack applications but my knowledge base spans way
+              beyond that, just ask!</div>
               <div className='copy' id='learn-more'>Learn more about what I do</div>
               <div className='copy' id='down-arrow'><a href='/about'>&or;</a></div>
               <MernLogos />
