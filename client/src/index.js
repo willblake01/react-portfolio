a6691f93c6e3b77221b78326d3e60ff2438c7564
William Blake <willblakebooking@gmail.com>
2018-05-30 13:05:23 -0500

    getting ready for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/index.css';
 import Router from './components/Router';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-registerServiceWorker();
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,10 @@ const db = require('../models');
 
 module.exports = function(app) {
 
+  app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   app.post('/contact/create', function(req, res) {
     db.user.create({
       name: req.body.name,
