7f491d63ef9e2c63ffae4a1e8abd5358e1d04521
William Blake <willblakebooking@gmail.com>
2018-10-03 11:23:03 -0500

    add winston logging

---
+++ log/lib/logger.js
@@ -0,0 +1,10 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  transports: [new(winston.transports.Console)({
+    timestamp: new Date().toISOString(),
+    colorize: true,
+  })],
+});
+
+module.exports = logger;
